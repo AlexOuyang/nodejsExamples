d429a6d769b5780c6cdb547a004f4fb66803d761
AlexOuyang <ouyangchenxing@hotmail.com>
2016-12-29 23:04:49 -0800

    implemented add and remove note functionality

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,10 +1,38 @@
-const fs = require('fs');
+const fs = require('fs'); // file system utilities
 const os = require('os');
-const _ = require('lodash');
+const _ = require('lodash'); // lodash module has a lot of good utilities functions
+const yargs = require('yargs'); // yargs module helps with commandline argument validation and key value pair args
 const notes = require('./notes.js');
 
-var res = notes.addNote();
-console.log(_.isString('feaf'));
+const argv = yargs.argv;
+
+var command = argv._[0];
+
+if (command === 'add') {
+    var note = notes.addNote(argv.title, argv.body); // usage: node app.js add --title="title" --body="body"
+    if (note) {
+        console.log('Note created');
+        console.log('--');
+        console.log('Title: ' + note.title);
+        console.log('Body: ' + note.body);
+    } else {
+        console.log('Note title taken');
+    }
+} else if (command === 'list') {
+    notes.getAll();
+} else if (command === 'read') {
+    notes.getNote(argv.title);
+} else if (command === 'remove') {
+    var removed = notes.removeNote(argv.title);
+    if (removed) {
+        console.log("Removed note");
+    } else {
+        console.log("Note not found");
+    }
+} else {
+    console.log('Command not found');
+}
+
 
 // var user = os.userInfo();
 
--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,4 +1,52 @@
-module.exports.addNote = () => {
-    console.log("add note");
-    return "New Note";
-}
+
+const fs = require('fs');
+
+var fetchNotes = () => {
+    try {
+        var notesString = fs.readFileSync('notes-data.json');
+        return JSON.parse(notesString);
+    } catch (e) {
+        return [];
+    }
+};
+
+var saveNotes = (notes) => {
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+};
+
+var addNote = (title, body) => {
+    // Bring out the saved notes the append the newly added note
+    var notes = fetchNotes();
+    var note = { title, body };
+
+    // duplicateNotes returns true if a note with title is found
+    var duplicateNotes = notes.filter((note) => note.title === title);
+
+    if (duplicateNotes.length === 0) {
+        notes.push(note);
+        saveNotes(notes);
+        return note;
+    }
+};
+
+var getAll = () => {
+    console.log('Getting all notes');
+};
+
+var getNote = (title) => {
+    console.log('Getting note', title);
+};
+
+var removeNote = (title) => {
+    var notes = fetchNotes();
+    var filtered = notes.filter((note) => note.title !== title);
+    saveNotes(filtered);
+    return notes.length !== filtered.length;
+};
+
+module.exports = {
+    addNote,
+    getAll,
+    getNote,
+    removeNote
+};
